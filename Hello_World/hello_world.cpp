e66a87e192298d552266f3f941564295167a4b19
Barış İşbiliroğlu <44321535+Bisbiliroglu@users.noreply.github.com>
2019-02-18 23:23:34 +0300

    Initial commit

---
+++ Hello_World/hello_world.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h> 
+
+ int main(void)
+ {
+ 	printf("Merhaba Dunya");
+ 	
+ 	printf("\n\n________________________________________________________________\nPress any key to exit\n");
+	getch();
+	return 0;
+ }
